0d80de582802d6dc27b643f6962c8d670791068c
David Ames <david.ames@canonical.com>
2016-05-04 15:02:03 -0700

    Pass optional settings to identity-credentials
    Allow config options to set optional settings on the
    identity-credentials relation.

--- reactive/client.py
+++ reactive/client.py
@@ -1,4 +1,4 @@
-from charmhelpers.core.hookenv import status_set, log
+from charmhelpers.core.hookenv import status_set, log, config
 from charmhelpers.core.templating import render
 from charms.reactive import when, when_not
 from charms.reactive import set_state, remove_state
@@ -7,7 +7,12 @@ from charms.reactive import set_state, remove_state
 @when('identity-credentials.connected')
 @when_not('identity-credentials.available.auth')
 def join_credentials_relation(keystone_credentials):
-    keystone_credentials.request_credentials(username='mock-client')
+    keystone_credentials.request_credentials(
+        username='mock-client',
+        project=config('project'),
+        requested_roles=config('requested_roles'),
+        requested_grants=config('requested_roles'),
+        domain=config('domain'))
     remove_state('credentials-ready')
     status_set('waiting', 'Waiting for identity credentials relation data')
 
